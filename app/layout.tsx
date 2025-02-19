8ea480b0da34292eed00a94a292c1ce5ba24ff26
Eniolayo <eniolayodeji@gmail.com>
2025-02-19 18:50:57 +0100

    Add SpeedInsights component and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -108,6 +109,7 @@ export default function RootLayout({
       >
         <main>{children}</main>
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
